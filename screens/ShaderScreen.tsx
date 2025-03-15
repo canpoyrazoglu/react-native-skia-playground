766ba510ad87233e2e33785eababa9ebae2b086e
Can Poyrazoğlu <can@canpoyrazoglu.com>
2025-03-15 19:17:47 +0300

    feat: implement bottom padding for keyboard

--- screens/ShaderScreen.tsx
+++ screens/ShaderScreen.tsx
@@ -1,18 +1,32 @@
+import { useDeviceOrientation } from '@react-native-community/hooks';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { Canvas, Fill, Shader, SkRuntimeEffect, useClock } from '@shopify/react-native-skia';
+import {
+  Canvas,
+  Fill,
+  Shader,
+  SkRuntimeEffect,
+  useClock,
+} from '@shopify/react-native-skia';
 import React, { useMemo, useState } from 'react';
-import { LayoutChangeEvent, StyleSheet, View } from 'react-native';
-import { useDerivedValue } from 'react-native-reanimated';
+import { LayoutChangeEvent, StyleSheet } from 'react-native';
+import Animated, {
+  useAnimatedKeyboard,
+  useAnimatedStyle,
+  useDerivedValue,
+} from 'react-native-reanimated';
 import ShaderInput from '../components/ShaderInput';
 import { RootStackParamList } from '../util/navigation/Navigation';
 
-
-function ShaderScreen(props: NativeStackScreenProps<RootStackParamList, 'Shader'>) {
+function ShaderScreen(
+  props: NativeStackScreenProps<RootStackParamList, 'Shader'>,
+) {
   const [shaderSource, setShaderSource] = useState<SkRuntimeEffect>();
-  const [size, setSize] = useState([0,0]);
+  const { height: keyboardHeight } = useAnimatedKeyboard();
+  const [size, setSize] = useState([0, 0]);
   const time = useClock();
   const [tapTime, setTapTime] = useState(-1);
-  const [tap, setTap] = useState([0,0]);
+  const [tap, setTap] = useState([0, 0]);
+  const orientation = useDeviceOrientation();
   const uniforms = useDerivedValue(() => {
     return {
       size,
@@ -21,35 +35,57 @@ function ShaderScreen(props: NativeStackScreenProps<RootStackParamList, 'Shader'
       tap,
     };
   });
-  const memoizedInput = useMemo(() => <ShaderInput style={styles.input} initialShader={props.route.params?.initialShader?.trim()} onShaderUpdated={setShaderSource} />, [props.initialShader]);
 
-  const onCanvasLayout = (e:LayoutChangeEvent) => {
+  const memoizedInput = useMemo(
+    () => (
+      <ShaderInput
+        style={styles.input}
+        initialShader={props.route.params?.initialShader?.trim()}
+        onShaderUpdated={setShaderSource}
+      />
+    ),
+    [props.route.params?.initialShader],
+  );
+
+  const onCanvasLayout = (e: LayoutChangeEvent) => {
     setSize([e.nativeEvent.layout.width, e.nativeEvent.layout.height]);
   };
-  return <View style={styles.container}>
-    <Canvas onLayout={onCanvasLayout} style={styles.canvas}>
-      <Fill>
-         { shaderSource && <Shader source={shaderSource} uniforms={uniforms} />}
-      </Fill>
-    </Canvas>
-    {memoizedInput}
-  </View>;
+  const containerStyle = useAnimatedStyle(() => {
+    return {
+      paddingBottom: keyboardHeight.value,
+    };
+  });
+  return (
+    <Animated.View style={[styles.container, containerStyle]}>
+      <Canvas
+        onLayout={onCanvasLayout}
+        style={[
+          styles.canvas,
+          orientation === 'landscape' ? styles.landscapeCanvas : undefined,
+        ]}>
+        <Fill>
+          {shaderSource && <Shader source={shaderSource} uniforms={uniforms} />}
+        </Fill>
+      </Canvas>
+      {memoizedInput}
+    </Animated.View>
+  );
 }
 
-
 const styles = StyleSheet.create({
-  container:{
+  container: {
     ...StyleSheet.absoluteFillObject,
   },
   canvas: {
-    backgroundColor:'#000',
+    backgroundColor: '#000',
     flex: 1,
   },
   input: {
     padding: 20,
-    paddingBottom: 40,
+    paddingBottom: 30,
     maxHeight: 200,
   },
+  landscapeCanvas: {},
 });
 
 export default ShaderScreen;
