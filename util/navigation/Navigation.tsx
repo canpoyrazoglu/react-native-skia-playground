c6058ba4e9cebc246159db96146395f300fbc2ab
Can Poyrazoğlu <can@canpoyrazoglu.com>
2025-03-16 20:03:57 +0300

    refactor: update navigation types/structure

--- util/navigation/Navigation.tsx
+++ util/navigation/Navigation.tsx
@@ -1,16 +1,18 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import HomeScreen from '../../screens/HomeScreen';
 import ShaderScreen from '../../screens/ShaderScreen';
 import theme from './theme';
 
-
 export type RootStackParamList = {
   Home: undefined;
-  Shader: {
-    initialShader?: string
-  } | undefined;
+  Shader:
+    | {
+        initialShader?: string;
+      }
+    | undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -21,15 +23,18 @@ function RootStack() {
       <Stack.Screen
         name="Home"
         component={HomeScreen}
+        options={{
+          title: 'Shader Playground',
+        }}
       />
-      <Stack.Screen
-        name="Shader"
-        component={ShaderScreen}
-      />
+      <Stack.Screen name="Shader" component={ShaderScreen} />
     </Stack.Navigator>
   );
 }
-export default () => <NavigationContainer theme={theme}>
-    <RootStack />
-</NavigationContainer>;
-
+export default () => (
+  <NavigationContainer theme={theme}>
+    <GestureHandlerRootView>
+      <RootStack />
+    </GestureHandlerRootView>
+  </NavigationContainer>
+);
