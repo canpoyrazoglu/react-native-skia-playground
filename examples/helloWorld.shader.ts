5c58c98a1bf2b2302e5c10da38dbf5d2f598c02b
Can Poyrazoğlu <can@canpoyrazoglu.com>
2025-03-16 20:03:12 +0300

    feat: update examples

--- examples/helloWorld.shader.ts
+++ examples/helloWorld.shader.ts
@@ -1,8 +1,5 @@
 export const HELLO_WORLD = `
 vec4 main(vec2 pos) {
-  // output color is vector of red, green, blue,
-  // and 4th valuenot used in this example
-  // this just renders a straight pink solid
-  // go ahead and play with the values below
+  /* output color is vector of red, green, blue, and 4th value not used in this example this just renders a straight pink solid go ahead and play with the values below */
   return vec4(1, 0.5, 0.8, 1);
 }`;
--- examples/timeGradient.shader.ts
+++ examples/timeGradient.shader.ts
@@ -1,6 +1,6 @@
 export const TIME_GRADIENT = `
 vec4 main(vec2 pos) {
-  // size and time are injected uniforms
+  /** size and time are injected uniforms red and green are normalized values from 0 to 1 at x and y axes respectively, and blue value is osciallating using sine of the injected uniform time */
   vec2 normalized = pos/size;
-  return vec4(normalized.x, normalized.y, sin(time * 10e-4), 1);
+  return vec4(normalized.x, normalized.y, 0.5 + sin(time * 10e-4) / 2, 1);
 }`;
