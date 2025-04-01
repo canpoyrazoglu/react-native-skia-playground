d2f2aef4f4cec176cfbfd61137ca206641e9de95
Can Poyrazoğlu <can@canpoyrazoglu.com>
2025-04-01 20:28:30 +0300

    feat: redesign home screen with descriptive list of shaders

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,33 +1,78 @@
 import React from 'react';
-import { Button, ScrollView, StyleSheet, View } from 'react-native';
-import exampleShaders from '../examples/shaders';
+import {
+  FlatList,
+  Image,
+  ListRenderItem,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import exampleShaders, {
+  EXAMPLE_THUMBS,
+  ShaderExample,
+} from '../examples/shaders';
 import { useNavigate } from '../hooks/navigation';
 
 function HomeScreen() {
   const navigate = useNavigate();
-  return (
-    <View style={styles.container}>
-      <ScrollView>
-        <View>
-          {exampleShaders.map((exampleShader, index) => (
-            <Button
-              title={exampleShader.name}
-              key={index}
-              onPress={() => {
-                navigate('Shader', {
-                  initialShader: exampleShader.shader,
-                });
-              }}
-            />
-          ))}
+
+  const renderItem: ListRenderItem<ShaderExample> = ({ item }) => {
+    return (
+      <Pressable
+        style={styles.item}
+        onPress={() => {
+          navigate('Shader', {
+            initialShader: item.shader,
+          });
+        }}>
+        <Image source={EXAMPLE_THUMBS[item.thumb]} style={styles.itemThumb} />
+        <View style={styles.textContainer}>
+          <Text style={styles.itemTitle}>{item.name}</Text>
+          <Text style={styles.itemDescription}>{item.description}</Text>
         </View>
-      </ScrollView>
+      </Pressable>
+    );
+  };
+  return (
+    <View style={StyleSheet.absoluteFillObject}>
+      <FlatList
+        contentContainerStyle={styles.list}
+        renderItem={renderItem}
+        data={exampleShaders}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  item: {
+    flexDirection: 'row',
+    height: 80,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+  itemThumb: {
+    width: 50,
+    height: 60,
+    borderRadius: 8,
+    marginRight: 20,
+  },
+  itemTitle: {
+    fontSize: 18,
+    color: '#ddb',
+    fontWeight: '400',
+  },
+  list: {
+    paddingVertical: 20,
+  },
+  itemDescription: {
+    color: '#aaa',
+    fontSize: 14,
+  },
+  textContainer: {
+    flex: 1,
+  },
 });
 
 export default HomeScreen;
