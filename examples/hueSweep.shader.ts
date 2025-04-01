cf06078cc0f7f7914d476a29fcadfbbe119d89e1
Can Poyrazoğlu <can@canpoyrazoglu.com>
2025-04-01 20:30:03 +0300

    chore: comment hue sweep shader

--- examples/hueSweep.shader.ts
+++ examples/hueSweep.shader.ts
@@ -1,4 +1,6 @@
 export const HUE_SWEEP = `
 vec4 main(vec2 pos) {
+  /** oscillate red, green, and blue channels with different periods to
+   *  create a "pseudorandom" color sweep effect
   return vec4(sin(time / 100), cos(time / 203), sin(time / 307), 1);
 }`;
