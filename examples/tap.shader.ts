d4b1b3c543bb421eceec33793c8e8ffdfd48e08c
Can Poyrazoğlu <can@canpoyrazoglu.com>
2025-04-01 20:27:18 +0300

    chore: comment tap shader

--- examples/tap.shader.ts
+++ examples/tap.shader.ts
@@ -1,14 +1,21 @@
 export const TAP = `
 vec4 main(vec2 pos) {
-  for(int i = 0; i < 20; i++){
+  /** iterate all possible touches (not the number of touches, but the
+   * size of the touches array)
+   */
+  for(int i = 0; i < MAX_TOUCHES; i++){
+    // check the current index vs actual touch count (from uniform)
     if(i >= touchCount){
       break;
     }
+
+    // get distance of current pixel's location to tap location
     vec4 touch = touches[i];
     vec2 tapPoint = vec2(touch[TOUCH_X], touch[TOUCH_Y]);
     float dist = distance(pos, tapPoint);
     float distLower = 36;
     if(!isTouchDown(touch)){
+      // expand away the touch after release
       float timePassed = time - touch[TOUCH_END_TIME];
       distLower += timePassed * 0.2;
     }
