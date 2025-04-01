3a25a1be663a472258fb4711f50417edc64bd25e
Can Poyrazoğlu <can@canpoyrazoglu.com>
2025-04-01 20:28:01 +0300

    chore: update and comment shader preamble

--- util/shaders/preamble.ts
+++ util/shaders/preamble.ts
@@ -2,18 +2,21 @@
  * without explicitly adding
  */
 const preamble = `
-  const int TOUCH_X = 0;
-  const int TOUCH_Y = 1;
-  const int TOUCH_START_TIME = 2;
-  const int TOUCH_END_TIME = 3;
-  const float INVALID_TIME = -1.0;
+  const int TOUCH_X = 0; // touch X position of touch vector
+  const int TOUCH_Y = 1; // touch Y position of touch vector
+  const int TOUCH_START_TIME = 2; // touch start time of touch vector
+  const int TOUCH_END_TIME = 3; // touch end time of touch vector
+  const float INVALID_TIME = -1.0; // invalid time constant
+  const int MAX_TOUCHES = 20; // max possible touches
+  const int RANDOM_COUNT = 100; // number of random numbers injected
 
-  uniform float2 size;
-  uniform float time;
-  uniform float4 touches[20];
-  uniform float randoms[100];
-  uniform int touchCount;
+  uniform float2 size; // size of the canvas
+  uniform float time; // time since canvas rendering started
+  uniform float4 touches[MAX_TOUCHES]; // array of current touches
+  uniform float randoms[RANDOM_COUNT]; // array of injected randoms
+  uniform int touchCount; // actual touch count
 
+  /** helper function to check if a touch is currently down */
   bool isTouchDown(vec4 touch){
     return touch[TOUCH_START_TIME] != INVALID_TIME && touch[TOUCH_END_TIME] > time;
   }
